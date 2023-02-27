34e45d965a431ecdf01acbca5bb6179e6d218428
tinashediego <tinashediego@gmail.com>
2023-02-27 10:15:56 +0200

    swagger ui

---
+++ src/main/java/com/educative/ecommerce/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.educative.ecommerce.controller;
+
+import com.educative.ecommerce.common.ApiResponse;
+import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/category")
+public class
+CategoryController {
+    @Autowired
+    CategoryService categoryService;
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return new ResponseEntity<>(new ApiResponse(true, "a new category created"), HttpStatus.CREATED);
+    }
+    @GetMapping("/")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
+    }
+    @GetMapping("/findById/{categoryId}")
+    public Optional<Category> oneCategory(@PathVariable("categoryId") int categoryId) {
+        return categoryService.findOne(categoryId);
+    }
+    @PutMapping("/update/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category ) {
+        System.out.println("category id " + categoryId);
+        if (!categoryService.findById(categoryId)) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.NOT_FOUND);
+        }
+        categoryService.editCategory(categoryId, category);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{categoryId}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable("categoryId") int categoryId){
+        categoryService.removeCategory(categoryId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Category has been deleted"), HttpStatus.OK);
+    }
+}
